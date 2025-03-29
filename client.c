9fa54d7add46361f22b3120cdf15e28ab33f87e8
Milos Milicevic <126508847+MilosM1998@users.noreply.github.com>
2025-03-29 18:48:19 +0000

    oooop

--- client.c
+++ client.c
@@ -1,5 +1,14 @@
 #include "mini_talk.h"
+#include <signal.h>
+#include <unistd.h>
 
+volatile sig_atomic_t ack_received = 0;
+
+void handle_ack(int sig)
+{
+    (void)sig;
+    ack_received = 1;
+}
 
 static int	check_pid(char *pid)
 {
@@ -25,45 +34,63 @@ static int	check_pid(char *pid)
 }
 void send_char(pid_t pid, char c)
 {
-	int i;
+    int i;
 
-	i = 7;
-	while(i >= 0)
-	{
-		if((c >> i) & 1)
-			kill(pid, SIGUSR2);
-		else
-			kill(pid, SIGUSR1);
-		i--;
-		usleep(100);
-	}
+    i = 7;
+    while (i >= 0)
+    {
+        ack_received = 0;  // Resetujemo pre slanja bita
+
+        if ((c >> i) & 1)
+            kill(pid, SIGUSR2);
+        else
+            kill(pid, SIGUSR1);
+
+        // Čekamo da server potvrdi prijem bita
+        while (!ack_received)
+            usleep(50);
+
+        i--;
+    }
 }
 
 void send_msg(pid_t pid, char *str)
 {
-	while(*str)
-	{
-		send_char(pid, *str);
-		str++;
-	}
-	send_char(pid, '\0');
+    while (*str)
+    {
+        send_char(pid, *str);
+        str++;
+    }
+    send_char(pid, '\0'); // Šaljemo završni karakter
 }
-int	main(int ac, char **av)
-{
-	pid_t	pid;
 
-	if (ac != 3)
-	{
-		ft_putendl_fd("Usage: ./client <PID> <MSG> ", 2);
-		return (1);
-	}
-	if (check_pid(av[1]))
-		return (1);
-	pid = (pid_t)ft_atoi(av[1]);
-	if (pid <= 0)
-	{
-		ft_putendl_fd("Error: Invalid pid.", 2);
-		return (1);
-	}
-	send_msg(pid, av[2]);
+int main(int ac, char **av)
+{
+    pid_t pid;
+    struct sigaction sa;
+
+    if (ac != 3)
+    {
+        ft_putendl_fd("Usage: ./client <PID> <MSG> ", 2);
+        return (1);
+    }
+
+    if (check_pid(av[1]))
+        return (1);
+    pid = (pid_t)ft_atoi(av[1]);
+    if (pid <= 0)
+    {
+        ft_putendl_fd("Error: Invalid pid.", 2);
+        return (1);
+    }
+
+    // Postavljanje handlera za primanje SIGUSR1 (ack)
+    sa.sa_handler = handle_ack;
+    sa.sa_flags = SA_RESTART;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGUSR1, &sa, NULL);
+
+    send_msg(pid, av[2]);
+
+    return (0);
 }
--- server.c
+++ server.c
@@ -1,41 +1,53 @@
 #include "mini_talk.h"
+#include <signal.h>
 #include <stdio.h>
+#include <unistd.h>
 
-void	take_msg(int sig)
+void take_msg(int sig, siginfo_t *info, void *context)
 {
-	static char	c = 0;
-	static int	i = 0;
+    static char c = 0;
+    static int i = 0;
 
-	c <<= 1;
-	if (sig == SIGUSR2)
-		c |= 1;
-	i++;
-	if (i == 8)
-	{
-		ft_putchar_fd(c, 1);
-		if (c == '\0')
-			ft_putchar_fd('\n', 1);
-		i = 0;
-		c = 0;
-	}
+    (void)context; // Da izbegnemo warning
+
+    c <<= 1;
+    if (sig == SIGUSR2)
+        c |= 1;
+    i++;
+
+    if (i == 8)
+    {
+        ft_putchar_fd(c, 1);
+        if (c == '\0')
+            ft_putchar_fd('\n', 1);
+        i = 0;
+        c = 0;
+    }
+
+    // Šaljemo potvrdu klijentu da smo primili bit
+    kill(info->si_pid, SIGUSR1);
 }
 
-int	main(void)
+int main(void)
 {
-	struct sigaction	sa;
-	char				*pid_str;
+    struct sigaction sa;
+    char *pid_str;
 
-	pid_str = ft_itoa(getpid());
-	ft_putstr_fd("Server PID is: ", 1);
-	ft_putstr_fd(pid_str, 1);
-	ft_putchar_fd('\n', 1);
-	free(pid_str);
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	sa.sa_handler = take_msg;
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
-	while (1)
-		pause();
-	return (0);
+    pid_str = ft_itoa(getpid());
+    ft_putstr_fd("Server PID is: ", 1);
+    ft_putstr_fd(pid_str, 1);
+    ft_putchar_fd('\n', 1);
+    free(pid_str);
+
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_SIGINFO; // Omogućava pristup info o pošiljaocu
+    sa.sa_sigaction = take_msg;
+
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+
+    while (1)
+        pause();
+    
+    return (0);
 }
